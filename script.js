d1d2c2cfd8bdbda9fb355bbfd50f487a3e953425
SamudraWijaya <ossasam919@gmail.com>
2022-08-16 15:24:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction(){
+    if (window.pageYOffset > 95) {
+        backToTopButton.style.display = "block";
+    }
+    else{
+        backToTopButton.style.display = "none";
+    }
+}
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop(){
+    window.scrollTo(0, 0);
+}
